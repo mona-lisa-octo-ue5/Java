18b8867e336b680848c904c66a00cc90aede57e3
石玉龙 <iosshiyulong@yeah.net>
2021-01-08 16:36:35 +0800

    创建对象 对象是根据类创建的。在Java中，使用关键字 new 来创建一个新的对象。创建对象需要以下三步： 声明：声明一个对象，包括对象名称和对象类型。 实例化：使用关键字 new 来创建一个对象。 初始化：使用 new 创建对象时，会调用构造方法初始化对象。

--- src/com/java/pro/Puppy.java
+++ src/com/java/pro/Puppy.java
@@ -5,6 +5,10 @@ public class Puppy {
 
     }
     public Puppy(String name) {
+        System.out.println("小狗的名字是："+name);
+    }
 
+    public static void main(String[] args) {
+        Puppy p = new Puppy("郭金龙");
     }
 }
