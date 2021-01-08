e369610a36cb39294ca416a78f3206dcc58196a1
石玉龙 <iosshiyulong@yeah.net>
2021-01-08 17:37:47 +0800

    强制类型转换 1. 条件是转换的数据类型必须是兼容的。 2. 格式：(type)value type是要强制类型转换后的数据类型 实例：

---
+++ src/com/java/pro/QiangZhiZhuanHuan.java
@@ -0,0 +1,9 @@
+package com.java.pro;
+
+public class QiangZhiZhuanHuan {
+    public static void main(String[] args) {
+        int i1=123;
+        byte b=(byte) i1;
+        System.out.println("int强制类型转换为byte后的值等于"+b);
+    }
+}
