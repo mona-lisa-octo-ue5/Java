b7fa2d3d23d625aedd224ebdcb75d6aedb098ceb
石玉龙 <iosshiyulong@yeah.net>
2021-01-08 17:19:00 +0800

    Java 基本数据类型 变量就是申请内存来存储值。也就是说，当创建变量的时候，需要在内存中申请空间。 内存管理系统根据变量的类型为变量分配存储空间，分配的空间只能用来储存该类型数据。
    因此，通过定义不同类型的变量，可以在内存中储存整数、小数或者字符。
    Java 的两大数据类型:
    内置数据类型
    引用数据类型

---
+++ src/com/java/pro/PrimitiveTypeTest.java
@@ -0,0 +1,42 @@
+package com.java.pro;
+
+public class PrimitiveTypeTest {
+    public static void main(String[] args) {
+        System.out.println("基本类型：byte 二进制位数："+Byte.SIZE);
+        System.out.println("包装类：java.lang.Byte");
+        System.out.println("最小值 Byte.MIN_VALUE="+Byte.MIN_VALUE);
+        System.out.println("最大值 Byte.MAX_VALUE="+Byte.MAX_VALUE);
+        System.out.println();
+        System.out.println("基本类型：short 二进制位数："+Short.SIZE);
+        System.out.println("包装类：java.lang.Short");
+        System.out.println("最小值：Short.MIN_VALUE="+Short.MIN_VALUE);
+        System.out.println("最小值：Short.MAX_VALUE="+Short.MAX_VALUE);
+        System.out.println();
+        System.out.println("基本类型：int 二进制位数：" + Integer.SIZE);
+        System.out.println("包装类：java.lang.Integer");
+        System.out.println("最小值：Integer.MIN_VALUE=" + Integer.MIN_VALUE);
+        System.out.println("最大值：Integer.MAX_VALUE=" + Integer.MAX_VALUE);
+        System.out.println();
+        System.out.println("基本类型：long 二进制位数：" + Long.SIZE);
+        System.out.println("包装类：java.lang.Long");
+        System.out.println("最小值：Long.MIN_VALUE=" + Long.MIN_VALUE);
+        System.out.println("最大值：Long.MAX_VALUE=" + Long.MAX_VALUE);
+        System.out.println();
+        System.out.println("基本类型：float 二进制位数：" + Float.SIZE);
+        System.out.println("包装类：java.lang.Float");
+        System.out.println("最小值：Float.MIN_VALUE=" + Float.MIN_VALUE);
+        System.out.println("最大值：Float.MAX_VALUE=" + Float.MAX_VALUE);
+        System.out.println();
+        System.out.println("基本类型：double 二进制位数：" + Double.SIZE);
+        System.out.println("包装类：java.lang.Double");
+        System.out.println("最小值：Double.MIN_VALUE=" + Double.MIN_VALUE);
+        System.out.println("最大值：Double.MAX_VALUE=" + Double.MAX_VALUE);
+        System.out.println();
+        System.out.println("基本类型：char 二进制位数：" + Character.SIZE);
+        System.out.println("包装类：java.lang.Character");
+        System.out.println("最小值：Character.MIN_VALUE="
+                + (int) Character.MIN_VALUE);
+        System.out.println("最大值：Character.MAX_VALUE="
+                + (int) Character.MAX_VALUE);
+    }
+}
