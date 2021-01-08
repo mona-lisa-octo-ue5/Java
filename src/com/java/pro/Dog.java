af4e7dee1329a62d0aab2d57914c11fcf86e752e
石玉龙 <iosshiyulong@yeah.net>
2021-01-08 16:24:36 +0800

    Java 对象和类 Java作为一种面向对象语言。支持以下基本概念： 多态 继承 封装 抽象 类 对象 实例 方法 重载

---
+++ src/com/java/pro/Dog.java
@@ -0,0 +1,25 @@
+package com.java.pro;
+
+public class Dog {
+    String breed;
+    int size;
+    String colour;
+    int age;
+    void eat() {
+        System.out.println("吃郭金龙");
+    }
+    void run() {
+
+    }
+    void sleep() {
+
+    }
+    void name() {
+
+    }
+
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.eat();
+    }
+}
