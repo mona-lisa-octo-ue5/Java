3ee0c938db2aefb36ebcddb267a07fddd15768b7
石玉龙 <iosshiyulong@yeah.net>
2021-01-08 16:40:13 +0800

    访问实例变量和方法 通过已创建的对象来访问成员变量和成员方法，如下所示： /* 实例化对象 */ Object referenceVariable = new Constructor(); /* 访问类中的变量 */ referenceVariable.variableName; /* 访问类中的方法 */ referenceVariable.methodName();

--- src/com/java/pro/Puppy.java
+++ src/com/java/pro/Puppy.java
@@ -1,6 +1,14 @@
 package com.java.pro;
 
 public class Puppy {
+    int puppyAge;
+    public void setAge(int age){
+        puppyAge=age;
+    }
+    public int getAge(){
+        System.out.println("小狗的年龄为："+puppyAge);
+        return puppyAge;
+    }
     public Puppy() {
 
     }
@@ -10,5 +18,8 @@ public class Puppy {
 
     public static void main(String[] args) {
         Puppy p = new Puppy("郭金龙");
+        p.setAge(2);
+        p.getAge();
+        System.out.println("变量值："+p.puppyAge);
     }
 }
