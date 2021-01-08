b638420f349e9a7bda9538404cc84d5fbeb24548
石玉龙 <iosshiyulong@yeah.net>
2021-01-08 17:23:32 +0800

    类型默认值 下表列出了 Java 各个类型的默认值：

---
+++ src/com/java/pro/ValueTest.java
@@ -0,0 +1,25 @@
+package com.java.pro;
+
+public class ValueTest {
+    static boolean bool;
+    static byte by;
+    static char ch;
+    static double d;
+    static float f;
+    static int i;
+    static long l;
+    static short sh;
+    static String str;
+
+    public static void main(String[] args) {
+        System.out.println("Bool:"+bool);
+        System.out.println("Byte:"+by);
+        System.out.println("Character:"+ch);
+        System.out.println("Double:"+d);
+        System.out.println("Float:"+f);
+        System.out.println("Integer :" + i);
+        System.out.println("Long :" + l);
+        System.out.println("Short :" + sh);
+        System.out.println("String :" + str);
+    }
+}
