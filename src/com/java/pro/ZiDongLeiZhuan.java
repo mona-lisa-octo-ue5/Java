cc760aacc3fcf536321ac51cfc9c54c55e789724
石玉龙 <iosshiyulong@yeah.net>
2021-01-08 17:35:15 +0800

    自动类型转换 必须满足转换前的数据类型的位数要低于转换后的数据类型，例如: short数据类型的位数为16位，就可以自动转换位数为32的int类型，同样float数据类型的位数为32，可以自动转换为64位的double类型。

---
+++ src/com/java/pro/ZiDongLeiZhuan.java
@@ -0,0 +1,12 @@
+package com.java.pro;
+
+public class ZiDongLeiZhuan {
+    public static void main(String[] args) {
+        char c1='a';
+        int i1=c1;
+        System.out.println("char自动类型转换为int后的值等于"+i1);
+        char c2='A';
+        int i2=c2+1;
+        System.out.println("char类型和int计算后的值等于"+i2);
+    }
+}
