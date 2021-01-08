810a515f69941be62a78e9044653e03cd0bf57f3
石玉龙 <iosshiyulong@yeah.net>
2021-01-08 18:26:14 +0800

    类变量（静态变量

--- src/com/java/pro/EmployeePro.java
+++ src/com/java/pro/EmployeePro.java
@@ -1,6 +1,8 @@
 package com.java.pro;
 
 public class EmployeePro {
+    private static double income;
+    public static final String Developer="渣渣工程师";
     public String name;
     private double salary;
     public EmployeePro(String ename){
@@ -18,5 +20,7 @@ public class EmployeePro {
         EmployeePro ep= new EmployeePro("郭狗狗");
         ep.setSalary(100.0);
         ep.printEmp();
+        income=100.0;
+        System.out.println(Developer+"平均工资"+income);
     }
 }
