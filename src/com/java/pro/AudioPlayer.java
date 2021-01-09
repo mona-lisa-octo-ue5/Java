06aa08c10c6950c2025519bbc0e68f219be97d81
石玉龙 <iosshiyulong@yeah.net>
2021-01-09 22:03:43 +0800

    子类能访问 protected 修饰符声明的方法和变量，这样就能保护不相关的类使用这些方法和变量。 下面的父类使用了 protected 访问修饰符，子类重写了父类的 openSpeaker() 方法。

---
+++ src/com/java/pro/AudioPlayer.java
@@ -0,0 +1,13 @@
+package com.java.pro;
+
+class AudioPlayer {
+    protected boolean openSpeaker(Speaker sp){
+
+    }
+}
+class StreamingAudioPlayer extends AudioPlayer{
+    @Override
+    protected boolean openSpeaker(Speaker sp) {
+        return super.openSpeaker(sp);
+    }
+}
