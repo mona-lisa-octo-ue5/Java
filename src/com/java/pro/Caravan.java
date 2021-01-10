7e28b65bbeaa5b0ac873e86f0b8187cef2cd72a3
石玉龙 <iosshiyulong@yeah.net>
2021-01-10 21:54:40 +0800

    abstract 修饰符

---
+++ src/com/java/pro/Caravan.java
@@ -0,0 +1,9 @@
+package com.java.pro;
+
+public class Caravan {
+    private double prioce;
+    private String model;
+    private String year;
+    public abstract void goFast(); //抽象方法
+    public abstract void changeColor();
+}
