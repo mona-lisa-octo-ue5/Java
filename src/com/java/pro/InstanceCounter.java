53a6a27e1ca874fd1416b88d3c575c366cf8b8b0
石玉龙 <iosshiyulong@yeah.net>
2021-01-10 21:09:52 +0800

    no message

--- src/com/java/pro/InstanceCounter.java
+++ src/com/java/pro/InstanceCounter.java
@@ -3,7 +3,7 @@ package com.java.pro;
 public class InstanceCounter {
     private static int numInstances=0;
     protected static int getCount(){
-        return numInstances
+        return numInstances;
     }
     private static void addInstance(){
         numInstances++;
@@ -13,6 +13,10 @@ public class InstanceCounter {
     }
 
     public static void main(String[] args) {
-        
+        System.out.println("Starting with"+InstanceCounter.getCount()+"instances");
+        for (int i=0;i<500;++i){
+            new InstanceCounter();
+        }
+        System.out.println("Created "+InstanceCounter.getCount()+" instances");
     }
 }
