3b42c7464806668900d2eadefbf724fbbb1a0479
石玉龙 <iosshiyulong@yeah.net>
2021-01-11 17:53:56 +0800

    定义日期格式的转换符可以使日期通过指定的转换符生成新字符串。这些日期转换符如下所示：

--- src/com/java/pro/JavaDateDemo.java
+++ src/com/java/pro/JavaDateDemo.java
@@ -2,7 +2,7 @@ package com.java.pro;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.logging.SimpleFormatter;
+import java.util.Locale;
 
 public class JavaDateDemo {
     public static void main(String[] args) {
@@ -19,6 +19,27 @@ public class JavaDateDemo {
         System.out.printf("HH:MM:SS PM格式（12时制）：%tr%n",date);
         System.out.printf("HH:MM:SS格式（24小时制）：%tT%n",date);
         System.out.printf("HH:MM格式（24小时制）：%tR",date);
+        System.out.println();
+        System.out.printf("%1$s %2$tB %2$td, %2$tY","Due date:",date);
+        System.out.println();
+        System.out.printf("%s %tB %<te, %<tY", "Due date:", date);
+        String str=String.format(Locale.US,"英文月份简称：%tb",date);
+        System.out.println(str);
+        System.out.printf("本地月份简称：%tb%n",date);
+        str=String.format(Locale.US,"英文月份全称：%tB",date);
+        System.out.println(str);
+        System.out.printf("本地月份全称：%tB%n",date);
+        str=String.format(Locale.US,"英文星期的简称：%ta",date);
+        System.out.println(str);
+        System.out.printf("本地星期的简称：%tA%n",date);
+        System.out.printf("年的前两位数字（不足两位前面补0）：%tC%n",date);
+        System.out.printf("年的后两位数字（不足两位前面补0）：%ty%n",date);
+        System.out.printf("一年中的天数（即年的第几天）：%tj%n",date);
+        System.out.printf("两位数字的月份（不足两位前面补0）：%tm%n",date);
+        System.out.printf("两位数字的日（不足两位前面补0）：%td%n",date);
+        System.out.printf("月份的日（前面不补0）：%te",date);
+
+
     }
 
 }
