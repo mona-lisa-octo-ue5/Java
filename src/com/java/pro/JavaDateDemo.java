78c4650d1f1960332fac96cba48bd194f43e1eed
石玉龙 <iosshiyulong@yeah.net>
2021-01-11 17:24:45 +0800

    使用 SimpleDateFormat 格式化日期

--- src/com/java/pro/JavaDateDemo.java
+++ src/com/java/pro/JavaDateDemo.java
@@ -1,11 +1,17 @@
 package com.java.pro;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.logging.SimpleFormatter;
 
 public class JavaDateDemo {
     public static void main(String[] args) {
         Date date = new Date();
         System.out.println(date.toString());
+        
+        Date dNow = new Date();
+        SimpleDateFormat ft=new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        System.out.println("当前时间为："+ft.format(dNow));
     }
 
 }
