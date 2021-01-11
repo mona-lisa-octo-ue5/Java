e6d1748473d9fd0903aeb5a8ac46a5682cbcdc44
石玉龙 <iosshiyulong@yeah.net>
2021-01-11 16:46:50 +0800

    数组作为函数的返回值

--- src/com/java/pro/JavaArrayDemo.java
+++ src/com/java/pro/JavaArrayDemo.java
@@ -44,7 +44,7 @@ public static void main(String[] args) {
 
     int[] array = {1,2,3,4,5,6,7,8,9,10};
     printArray(array);
-
+    reverse(array);
 
 }
 
@@ -54,5 +54,14 @@ public static void main(String[] args) {
         }
     }
 
+    public static int[] reverse(int[] list){
+        int[] result=new int[list.length];
+        for (int i=0,j=result.length-1; i<list.length; i++,j--){
+            result[j]=list[i];
+            System.out.println("result[j]="+result[j]);
+        }
+        System.out.println("result="+result);
+        return result;
+    }
 
 }
