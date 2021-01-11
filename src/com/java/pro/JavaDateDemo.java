8540087f0378758670f3f50e6f9970641f1a9e6e
石玉龙 <iosshiyulong@yeah.net>
2021-01-11 17:19:34 +0800

    获取当前日期时间

---
+++ src/com/java/pro/JavaDateDemo.java
@@ -0,0 +1,11 @@
+package com.java.pro;
+
+import java.util.Date;
+
+public class JavaDateDemo {
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println(date.toString());
+    }
+
+}
