f09d9c63419d37183a1c8081b521d3717b4e2eef
石玉龙 <iosshiyulong@yeah.net>
2021-01-11 10:05:30 +0800

    Java 运算符 计算机的最基本用途之一就是执行数学运算，作为一门计算机语言，Java也提供了一套丰富的运算符来操纵变量。我们可以把运算符分成以下几组： 算术运算符 关系运算符 位运算符 逻辑运算符 赋值运算符 其他运算符

--- src/com/java/pro/Caravan.java
+++ src/com/java/pro/Caravan.java
@@ -4,6 +4,21 @@ public class Caravan {
     private double prioce;
     private String model;
     private String year;
-    public abstract void goFast(); //抽象方法
-    public abstract void changeColor();
+
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        int c=25;
+        int d=25;
+        System.out.println("a+b="+(a+b));
+        System.out.println("a-b="+(a-b));
+        System.out.println("a*b="+(a*b));
+        System.out.println("b/a="+(b/a));
+        System.out.println("b%a="+(b%a));
+        System.out.println("c%a="+(c%a));
+        System.out.println("a++ ="+(a++));
+        System.out.println("a-- ="+(a--));
+        System.out.println("d++ ="+(d++));
+        System.out.println("++d ="+(++d));
+    }
 }
