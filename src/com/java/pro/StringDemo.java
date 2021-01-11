44cce8e51c25cb6291513ff77d054391d37d95fd
石玉龙 <iosshiyulong@yeah.net>
2021-01-11 13:51:49 +0800

    字符串长度

--- src/com/java/pro/StringDemo.java
+++ src/com/java/pro/StringDemo.java
@@ -5,7 +5,12 @@ public class StringDemo {
         char[] helloArray={'r','u','n','o','e','b'};
         String helloString=new String(helloArray);
         System.out.println(helloString);
-//        String site
-        
+
+        String site = "www.runoob.com";
+        int len = site.length();
+        System.out.println( "菜鸟教程网址长度 : " + len );
+
+
+
     }
 }
