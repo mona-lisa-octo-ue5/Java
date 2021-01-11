fd99b0e4acf4de397e02623b653d14bcc3141d55
石玉龙 <iosshiyulong@yeah.net>
2021-01-11 16:36:01 +0800

    数组作为函数的参数

--- src/com/java/pro/JavaArrayDemo.java
+++ src/com/java/pro/JavaArrayDemo.java
@@ -41,5 +41,18 @@ public static void main(String[] args) {
     for (double element: myList2) {
         System.out.println(element);
     }
+
+    int[] array = {1,2,3,4,5,6,7,8,9,10};
+    printArray(array);
+
+
 }
+
+    public static void printArray(int[] array){
+        for (int i=0; i<array.length; i++){
+            System.out.println(array[i]+" ");
+        }
+    }
+
+
 }
