686667904d4f390aac5425bd7a94d342e6ec1b58
石玉龙 <iosshiyulong@yeah.net>
2021-01-11 17:32:11 +0800

    使用printf格式化日期

--- src/com/java/pro/JavaDateDemo.java
+++ src/com/java/pro/JavaDateDemo.java
@@ -8,10 +8,17 @@ public class JavaDateDemo {
     public static void main(String[] args) {
         Date date = new Date();
         System.out.println(date.toString());
-        
+
         Date dNow = new Date();
         SimpleDateFormat ft=new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
         System.out.println("当前时间为："+ft.format(dNow));
+
+        System.out.printf("全部日期和时间信息：%tc%n",date);
+        System.out.printf("年-月-日格式：%tF%n",date);
+        System.out.printf("月/日/年格式：%tD%n",date);
+        System.out.printf("HH:MM:SS PM格式（12时制）：%tr%n",date);
+        System.out.printf("HH:MM:SS格式（24小时制）：%tT%n",date);
+        System.out.printf("HH:MM格式（24小时制）：%tR",date);
     }
 
 }
