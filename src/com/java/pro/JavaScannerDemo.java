c2db4874081a11c4e4984d00b5ffa5f1a3810a65
石玉龙 <iosshiyulong@yeah.net>
2021-01-12 17:08:03 +0800

    如果要输入 int 或 float 类型的数据，在 Scanner 类中也有支持，但是在输入之前最好先使用 hasNextXxx() 方法进行验证，再使用 nextXxx() 来读取：

--- src/com/java/pro/JavaScannerDemo.java
+++ src/com/java/pro/JavaScannerDemo.java
@@ -25,10 +25,31 @@ public class JavaScannerDemo {
         scan.close();
     }
 
-    public static void main(String[] args) {
-        scanner();
-        scannerNextLine();
+    public static void scannerDemo(){
+        Scanner scan=new Scanner(System.in);
+        int i=0;
+        float f=0.0f;
+        System.out.print("输入整数：");
+        if (scan.hasNextInt()){
+            i=scan.nextInt();
+            System.out.println("整数数据："+i);
+        }else {
+            System.out.println("输入的不是整数！");
+        }
+        System.out.print("输入小数：");
+        if (scan.hasNextFloat()){
+            f=scan.nextFloat();
+            System.out.println("小数数据："+f);
+        }else {
+            System.out.println("输入的不是小数！");
+        }
+        scan.close();
+    }
 
+    public static void main(String[] args) {
+//        scanner();
+//        scannerNextLine();
+        scannerDemo();
     }
 
 
