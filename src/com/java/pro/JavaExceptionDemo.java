de430e0006e2db7af9cef54d20ee009fcc8846dc
石玉龙 <iosshiyulong@yeah.net>
2021-01-12 18:01:20 +0800

    声明自定义异常

--- src/com/java/pro/JavaExceptionDemo.java
+++ src/com/java/pro/JavaExceptionDemo.java
@@ -19,11 +19,34 @@ public class JavaExceptionDemo {
         throw new RemoteException();
     }
 
+    public static void exceFinally(){
+        int a[]=new int[2];
+        try{
+            System.out.println("Access element three :"+a[3]);
+        }catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Exception throw :"+e);
+        }
+        finally {
+            a[0]=6;
+//            System.out.println("First element value: ",+ a[0]);
+            System.out.println("The finally statement is executed");
+        }
+    }
+
     public static void main(String[] args) {
         exce();
-        
-
+        exceFinally();
     }
 
 
+}
+
+class InsufficientFundsException extends Exception {
+    private double amount;
+    public InsufficientFundsException(double amount){
+        this.amount=amount;
+    }
+    public double getAmount(){
+        return amount;
+    }
 }
