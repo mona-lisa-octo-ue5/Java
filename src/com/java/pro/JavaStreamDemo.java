3b3d425253caf4812619317452bed0bc14025c67
石玉龙 <iosshiyulong@yeah.net>
2021-01-12 16:02:53 +0800

    上代码由于是二进制写入，可能存在乱码，你可以使用以下代码实例来解决乱码问题：

--- src/com/java/pro/JavaStreamDemo.java
+++ src/com/java/pro/JavaStreamDemo.java
@@ -56,11 +56,35 @@ public class JavaStreamDemo {
             System.out.println(e);
         }
     }
+    public static void fileStreamTest2(){
+        try {
+            File f=new File("a.txt");
+            FileOutputStream fop=new FileOutputStream(f);
+            OutputStreamWriter writer=new OutputStreamWriter(fop,"UTF-8");
+            writer.append("中文输入");
+            writer.append("\r\n");
+            writer.append("English");
+            writer.close();
+            fop.close();
+            FileInputStream fip=new FileInputStream(f);
+            InputStreamReader reader=new InputStreamReader(fip,"UTF-8");
+            StringBuffer sb=new StringBuffer();
+            while (reader.ready()){
+                sb.append((char) reader.read());
+            }
+            System.out.println(sb.toString());
+            reader.close();
+            fip.close();
+        }catch (Exception e){
+            System.out.println(e.getLocalizedMessage());
+        }
+    }
     public static void main(String[] args) {
 //        input();
 //        input2();
 //        write();
 //        fileStreamTest();
-        
+        fileStreamTest2();
+
     }
 }
