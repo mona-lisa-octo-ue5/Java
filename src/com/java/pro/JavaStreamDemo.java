9eea2fb75ce430a63b6ff6939502a3edc93d837d
石玉龙 <iosshiyulong@yeah.net>
2021-01-12 15:24:40 +0800

    一个演示 InputStream 和 OutputStream 用法的例子

--- src/com/java/pro/JavaStreamDemo.java
+++ src/com/java/pro/JavaStreamDemo.java
@@ -1,17 +1,20 @@
 package com.java.pro;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class JavaStreamDemo {
     public static void input(){
         char c;
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         System.out.println("输入字符，按下'q'键退出。");
-//        do{
-//            c = (char) br.read();
-//            System.out.println(c);
-//        }while (c != 'q');
+        try{
+            do{
+                c = (char) br.read();
+                System.out.println(c);
+            }while (c != 'q');
+        }catch (Exception e){
+            System.out.println(e.getLocalizedMessage());
+        }
     }
 
     public static void input2(){
@@ -19,10 +22,14 @@ public class JavaStreamDemo {
         String str;
         System.out.println("Enter lines of text.");
         System.out.println("Enter 'end' to quit.");
-//        do {
-//            str=br.readLine();
-//            System.out.println(str);
-//        }while (!str.equals("end"));
+        try{
+            do {
+                str=br.readLine();
+                System.out.println(str);
+            }while (!str.equals("end"));
+        }catch (Exception e){
+            System.out.println(e.getLocalizedMessage());
+        }
     }
     public static void write(){
         int b;
@@ -30,9 +37,29 @@ public class JavaStreamDemo {
         System.out.write(b);
         System.out.write('\n');
     }
+
+    public static void fileStreamTest(){
+        try{
+            byte bWrite[] ={11,21,3,40,5};
+            OutputStream os=new FileOutputStream("test.txt");
+            for (int x=0;x<bWrite.length;x++){
+                os.write(bWrite[x]);
+            }
+            os.close();
+            InputStream is=new FileInputStream("test.txt");
+            int size =is.available();
+            for (int i=0;i<size;i++){
+                System.out.print((char) is.read()+" ");
+            }
+            is.close();
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
     public static void main(String[] args) {
         input();
         input2();
         write();
+        fileStreamTest();
     }
 }
