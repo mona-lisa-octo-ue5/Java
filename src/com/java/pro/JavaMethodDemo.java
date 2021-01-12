9f4635c720fc2aae2484ce409418728d9bb5254f
石玉龙 <iosshiyulong@yeah.net>
2021-01-12 13:56:19 +0800

    void 关键字

--- src/com/java/pro/JavaMethodDemo.java
+++ src/com/java/pro/JavaMethodDemo.java
@@ -13,8 +13,23 @@ public class JavaMethodDemo {
         System.out.println(result);
         return result;
     }
+    public static void printGrade(double score){
+        if (score>=90.0){
+            System.out.println('A');
+        }else if (score<=80.0){
+            System.out.println('B');
+        }else if (score>=70.0){
+            System.out.println('C');
+        }else if (score>=60.0){
+            System.out.println('D');
+        }else {
+            System.out.println('F');
+        }
+        //学习方法：每天根据一个故事场景写一段中文。例如：妈妈做早餐给我吃。使用百度翻译App翻译为英文和德文。将每天积累的一段话都写在纸上贴在墙壁上。经常反复记忆朗读。
+    }
 
     public static void main(String[] args) {
         max(5,6);
+        printGrade(100);
     }
 }
