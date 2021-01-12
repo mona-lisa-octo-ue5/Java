3ccfcca971e5d0cb16ca2c7a21e70b3240e7b58c
石玉龙 <iosshiyulong@yeah.net>
2021-01-12 15:02:38 +0800

    从控制台读取字符串

--- src/com/java/pro/JavaStreamDemo.java
+++ src/com/java/pro/JavaStreamDemo.java
@@ -13,7 +13,20 @@ public class JavaStreamDemo {
 //            System.out.println(c);
 //        }while (c != 'q');
     }
+
+    public static void input2(){
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        String str;
+        System.out.println("Enter lines of text.");
+        System.out.println("Enter 'end' to quit.");
+//        do {
+//            str=br.readLine();
+//            System.out.println(str);
+//        }while (!str.equals("end"));
+    }
     public static void main(String[] args) {
         input();
+        input2();
+
     }
 }
