0c5b5a296822ff2244361d274b59d4fd9803b0fa
石玉龙 <iosshiyulong@yeah.net>
2021-01-13 09:44:15 +0800

    为什么需要继承

--- src/com/java/obj/JavaExtendsDemo.java
+++ src/com/java/obj/JavaExtendsDemo.java
@@ -4,9 +4,48 @@ package com.java.obj;
 class Persion{
 }
 class Student extends Persion {
-    
+
 }
 
 public class JavaExtendsDemo {
 
+
+    public static void main(String[] args) {
+        System.out.println("继承");
+    }
+}
+
+class Penguin{
+    private String name;
+    private int id;
+    public Penguin(String myName,int myid){
+        name=myName;
+        id=myid;
+    }
+    public void eat(){
+        System.out.println(name+"正在吃");
+    }
+    public void sleep(){
+        System.out.println(name+"正在睡");
+    }
+    public void introduction(){
+        System.out.println("大家好！我是"+id+"号"+name+".");
+    }
+}
+class Mouse{
+    private String name;
+    private int id;
+    public Mouse(String myName,int myid){
+        name=myName;
+        id=myid;
+    }
+    public void eat(){
+        System.out.println(name+"正在吃");
+    }
+    public void sleep(){
+        System.out.println(name+"正在睡");
+    }
+    public void introduction(){
+        System.out.println("大家好！我是"+id+"号"+name+".");
+    }
 }
