364a8a574c59e555a8a338bdef73702b05ab0b73
石玉龙 <iosshiyulong@yeah.net>
2021-01-13 15:06:42 +0800

    通过如下的例子说明EncapTest类的变量怎样被访问：

--- src/com/java/obj/JavaEncapsulationDemo.java
+++ src/com/java/obj/JavaEncapsulationDemo.java
@@ -3,7 +3,11 @@ package com.java.obj;
 public class JavaEncapsulationDemo {
 
     public static void main(String[] args) {
-
+        Encap encap=new Encap();
+        encap.setAge(20);
+        encap.setName("James");
+        encap.setIdNum("12345ms");
+        System.out.println("Name: "+encap.getName()+" Age: "+encap.getAge());
     }
 }
 class Encap{
