a6004dec2a03b265acaf87e7bec82e28b60bd0ae
石玉龙 <iosshiyulong@yeah.net>
2021-01-13 15:39:54 +0800

    内部类中使用枚举

--- src/com/java/obj/JavaEnumDemo.java
+++ src/com/java/obj/JavaEnumDemo.java
@@ -1,9 +1,16 @@
 package com.java.obj;
 
 public class JavaEnumDemo {
+
+    enum Color2{
+        RED,GREEN,BLUE;
+    }
+
     public static void main(String[] args) {
         Color c=Color.BLUE;
         System.out.println(c);
+        Color2 c2=Color2.RED;
+        System.out.println(c2);
     }
 }
 enum Color {
