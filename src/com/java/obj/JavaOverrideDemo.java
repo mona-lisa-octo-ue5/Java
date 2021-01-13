6ff845024a1251f5b770a363566fca4f1f911293
石玉龙 <iosshiyulong@yeah.net>
2021-01-13 11:06:14 +0800

    Super 关键字的使用

--- src/com/java/obj/JavaOverrideDemo.java
+++ src/com/java/obj/JavaOverrideDemo.java
@@ -4,8 +4,9 @@ public class JavaOverrideDemo {
     public static void main(String[] args) {
         Animal2 a=new Animal2();
         Animal2 b=new Dog();
-        a.move();
+//        a.move();
         b.move();
+//        b.bark();
     }
 }
 class Animal2{
@@ -15,6 +16,10 @@ class Animal2{
 }
 class Dog extends Animal2{
     public void move(){
+        super.move();
         System.out.println("狗可以跑和走");
+    }
+    public void bark(){
+        System.out.println("狗可以叫");
     }
 }
