e15c620751c7bf4012450119efc0c3c137165dd2
石玉龙 <iosshiyulong@yeah.net>
2021-01-13 15:04:17 +0800

    任何要访问类中私有成员变量的类都要通过这些getter和setter方法。

--- src/com/java/obj/JavaEncapsulationDemo.java
+++ src/com/java/obj/JavaEncapsulationDemo.java
@@ -2,6 +2,38 @@ package com.java.obj;
 
 public class JavaEncapsulationDemo {
 
+    public static void main(String[] args) {
+
+    }
+}
+class Encap{
+    private String name;
+    private String idNum;
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getIdNum() {
+        return idNum;
+    }
+
+    public void setAge(int newAge) {
+        age=newAge;
+    }
+
+    public void setName(String newName) {
+        name = newName;
+    }
+
+    public void setIdNum(String newIdNum) {
+        idNum = newIdNum;
+    }
 }
 class Person{
     private String name;
