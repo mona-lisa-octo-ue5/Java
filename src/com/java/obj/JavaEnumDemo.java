583ffa3aa81a144081e7533e33352a901124bc2f
石玉龙 <iosshiyulong@yeah.net>
2021-01-13 15:38:05 +0800

    Java 枚举(enum)

---
+++ src/com/java/obj/JavaEnumDemo.java
@@ -0,0 +1,11 @@
+package com.java.obj;
+
+public class JavaEnumDemo {
+    public static void main(String[] args) {
+        Color c=Color.BLUE;
+        System.out.println(c);
+    }
+}
+enum Color {
+    RED,GREEN,BLUE;
+}
