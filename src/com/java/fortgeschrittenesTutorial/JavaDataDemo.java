c5f473a72d632cf0e1e45f1068f466dcf90b27d7
石玉龙 <iosshiyulong@yeah.net>
2021-01-13 17:36:26 +0800

    Java Vector 类

--- src/com/java/fortgeschrittenesTutorial/JavaDataDemo.java
+++ src/com/java/fortgeschrittenesTutorial/JavaDataDemo.java
@@ -6,8 +6,40 @@ import java.util.Vector;
 
 public class JavaDataDemo {
     public static void main(String[] args) {
+        vector();
         bitset();
         enumeration();
+    }
+    public static void vector(){
+        Vector v=new Vector(3,2);
+        System.out.println("Initial size: "+v.size());
+        System.out.println("Initaial capacity: "+v.capacity());
+        v.addElement(new Integer(1));
+        v.addElement(new Integer(2));
+        v.addElement(new Integer(3));
+        v.addElement(new Integer(4));
+        System.out.println("Capacity after four additions: "+v.capacity());
+        v.addElement(new Double(5.45));
+        System.out.println("Current capacity: "+v.capacity());
+        v.addElement(new Double(6.08));
+        v.addElement(new Integer(7));
+        System.out.println("Current capacity: "+v.capacity());
+        v.addElement(new Float(9.4));
+        v.addElement(new Integer(10));
+        System.out.println("Current capacity: "+v.capacity());
+        v.addElement(new Integer(11));
+        v.addElement(new Integer(12));
+        System.out.println("First element: "+(Integer)v.firstElement());
+        System.out.println("Last element: "+(Integer)v.lastElement());
+        if (v.contains(new Integer(3))){
+            System.out.println("Vector contains 3.");
+        }
+        Enumeration vEnum=v.elements();
+        System.out.println("\n Elements in vector: ");
+        while (vEnum.hasMoreElements()){
+            System.out.print(vEnum.nextElement()+" ");
+        }
+        System.out.println();
     }
     public static void bitset(){
         BitSet bits1=new BitSet(16);
