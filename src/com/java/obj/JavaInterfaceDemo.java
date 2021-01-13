4887989372593a13fc10be45d349de50984735d3
石玉龙 <iosshiyulong@yeah.net>
2021-01-13 15:34:59 +0800

    接口的多继承

--- src/com/java/obj/JavaInterfaceDemo.java
+++ src/com/java/obj/JavaInterfaceDemo.java
@@ -19,3 +19,24 @@ public class JavaInterfaceDemo implements JavaInterface {
         System.out.println("海南三亚三日游！");
     }
 }
+interface Sports{
+    public void setHomeTeam(String name);
+    public void setVisitingTeam(String name);
+}
+interface Football extends Sports{
+    public void homeTeamScored(int points);
+    public void visitingTeamScored(int points);
+    public void endOfQuarter(int quarter);
+}
+interface Hockey extends Sports{
+    public void homeGoalScored();
+    public void visitingGoalScored();
+    public void endOfPeriod(int period);
+    public void overtimePeriod(int ot);
+}
+interface Event{
+    public void move();
+}
+interface robbot extends Event,Sports{
+
+}
