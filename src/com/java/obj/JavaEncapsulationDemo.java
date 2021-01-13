bd613be46a6563632bd8764d9bdb6937b6f94e7d
石玉龙 <iosshiyulong@yeah.net>
2021-01-13 15:00:45 +0800

    采用 this 关键字是为了解决实例变量（private String name）和局部变量（setName(String name)中的name变量）之间发生的同名的冲突。

--- src/com/java/obj/JavaEncapsulationDemo.java
+++ src/com/java/obj/JavaEncapsulationDemo.java
@@ -6,4 +6,20 @@ public class JavaEncapsulationDemo {
 class Person{
     private String name;
     private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
