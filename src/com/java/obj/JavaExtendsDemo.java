a9b6759fe63f4d048be24d91c8250cd86d3d9438
石玉龙 <iosshiyulong@yeah.net>
2021-01-13 09:54:20 +0800

    继承关键字

--- src/com/java/obj/JavaExtendsDemo.java
+++ src/com/java/obj/JavaExtendsDemo.java
@@ -15,6 +15,30 @@ public class JavaExtendsDemo {
     }
 }
 
+class Animal{
+    private String name;
+    private int id;
+    public Animal(String myName,int myid){
+        name=myName;
+        id=myid;
+    }
+    public void eat(){
+        System.out.println(name+"正在吃");
+    }
+    public void sleep(){
+        System.out.println(name+"正在睡");
+    }
+    public void introduction(){
+        System.out.println("大家好！我是"+id+"号"+name+".");
+    }
+}
+class Cat extends Animal{
+    public Cat(String myName,int myid){
+        super(myName,myid);
+    }
+}
+
+
 class Penguin{
     private String name;
     private int id;
