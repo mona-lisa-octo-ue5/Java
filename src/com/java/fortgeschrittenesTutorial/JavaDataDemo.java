dfc58de2fa6d077d6b89cdf505c7e2ae08d6ef0b
石玉龙 <iosshiyulong@yeah.net>
2021-01-13 17:06:43 +0800

    Java Bitset类

--- src/com/java/fortgeschrittenesTutorial/JavaDataDemo.java
+++ src/com/java/fortgeschrittenesTutorial/JavaDataDemo.java
@@ -1,11 +1,37 @@
 package com.java.fortgeschrittenesTutorial;
 
+import java.util.BitSet;
 import java.util.Enumeration;
 import java.util.Vector;
 
 public class JavaDataDemo {
     public static void main(String[] args) {
+        bitset();
         enumeration();
+    }
+    public static void bitset(){
+        BitSet bits1=new BitSet(16);
+        BitSet bits2=new BitSet(16);
+        for (int i=0;i<16;i++){
+            if ((i%2)==0) bits1.set(i);
+            if ((i%5)!=0) bits2.set(i);
+        }
+        System.out.println("Initial pattern in bits1: ");
+        System.out.println(bits1);
+        System.out.println("\n Initial pattern in bits2: ");
+        System.out.println(bits2);
+
+        bits2.and(bits1);
+        System.out.println("\n bits2 AND bits1");
+        System.out.println(bits2);
+
+        bits2.or(bits1);
+        System.out.println("\n bits2 OR bits1: ");
+        System.out.println(bits2);
+
+        bits2.xor(bits1);
+        System.out.println("\n bits2 XOR bits1: ");
+        System.out.println(bits2);
     }
     public static void enumeration(){
         Enumeration<String> days;
