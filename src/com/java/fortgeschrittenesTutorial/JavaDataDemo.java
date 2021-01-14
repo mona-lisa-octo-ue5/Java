029ffe2525f31effc9ce8b820bf139ec118ac2eb
石玉龙 <iosshiyulong@yeah.net>
2021-01-14 11:54:53 +0800

    Java Hashtable 类

--- src/com/java/fortgeschrittenesTutorial/JavaDataDemo.java
+++ src/com/java/fortgeschrittenesTutorial/JavaDataDemo.java
@@ -4,10 +4,31 @@ import java.util.*;
 
 public class JavaDataDemo {
     public static void main(String[] args) {
+        hashtable();
         map();
         vector();
         bitset();
         enumeration();
+    }
+    public static void hashtable(){
+        Hashtable balance=new Hashtable();
+        Enumeration names;
+        String str;
+        double bal;
+        balance.put("Zara",new Double(3434.34));
+        balance.put("Mahnaz",new Double(123.22));
+        balance.put("Ayan",new Double(1378.00));
+        balance.put("Daisy",new Double(99.22));
+        balance.put("Qadir",new Double(-19.08));
+        names=balance.keys();
+        while (names.hasMoreElements()){
+            str=(String) names.nextElement();
+            System.out.println(str+": "+balance.get(str));
+        }
+        System.out.println();
+        bal=((Double)balance.get("Zara")).doubleValue();
+        balance.put("Zara",new Double(bal+1000));
+        System.out.println("Zara's new balance: "+balance.get("Zara"));
     }
     public static void map(){
         Map m=new HashMap();
