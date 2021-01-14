8e3757320af9de91389270cfedb5b38d10059fca
石玉龙 <iosshiyulong@yeah.net>
2021-01-14 17:11:37 +0800

    反序列化对象 下面的 DeserializeDemo 程序实例了反序列化，/tmp/employee.ser 存储了 Employee 对象。

--- src/com/java/fortgeschrittenesTutorial/JavaSerializeDemo.java
+++ src/com/java/fortgeschrittenesTutorial/JavaSerializeDemo.java
@@ -7,6 +7,29 @@ import java.io.*;
 public class JavaSerializeDemo {
     public static void main(String[] args) {
         serialize();
+        deserialize();
+    }
+    public static void deserialize(){
+        JavaGenericsDemo e=null;
+        try{
+            FileInputStream fileIn=new FileInputStream("/tmp/employee.ser");
+            ObjectInputStream in=new ObjectInputStream(fileIn);
+            e=(JavaGenericsDemo) in.readObject();
+            in.close();
+            fileIn.close();
+        }catch (IOException i){
+            i.printStackTrace();
+            return;
+        }catch (ClassNotFoundException c){
+            System.out.println("Employ class not found");
+            c.printStackTrace();
+            return;
+        }
+        System.out.println("Deserialized Employee...");
+        System.out.println("Name: "+e.name);
+        System.out.println("Address: "+e.address);
+        System.out.println("SSN: "+e.SSN);
+        System.out.println("Number: "+e.number);
     }
     public static void serialize(){
         JavaGenericsDemo e=new JavaGenericsDemo();
