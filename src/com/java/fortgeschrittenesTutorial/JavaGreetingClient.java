27b8a67529d851796a5b281e9eb01adf6fb690eb
石玉龙 <iosshiyulong@yeah.net>
2021-01-14 18:15:44 +0800

    Socket 服务端实例

--- src/com/java/fortgeschrittenesTutorial/JavaGreetingClient.java
+++ src/com/java/fortgeschrittenesTutorial/JavaGreetingClient.java
@@ -1,11 +1,22 @@
 package com.java.fortgeschrittenesTutorial;
 
 import java.io.*;
+import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketTimeoutException;
 import java.util.SortedMap;
 
 public class JavaGreetingClient {
     public static void main(String[] args) {
+
+        int port2 =Integer.parseInt(args[0]);
+        try {
+            Thread t=new GreetingServer(port2);
+            t.run();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
         String serverName=args[0];
         int port = Integer.parseInt(args[1]);
         try{
@@ -21,6 +32,34 @@ public class JavaGreetingClient {
             client.close();
         }catch (IOException e){
             e.printStackTrace();
+        }
+    }
+}
+
+class GreetingServer extends Thread{
+    private ServerSocket serverSocket;
+    public GreetingServer(int port) throws IOException{
+        serverSocket=new ServerSocket(port);
+        serverSocket.setSoTimeout(10000);
+    }
+    public void run(){
+        while (true){
+            try{
+                System.out.println("等待远程连接，端口号为："+serverSocket.getLocalPort()+"...");
+                Socket server=serverSocket.accept();
+                System.out.println("远程主机地址： "+server.getRemoteSocketAddress());
+                DataInputStream in=new DataInputStream(server.getInputStream());
+                System.out.println(in.readUTF());
+                DataOutputStream out=new DataOutputStream(server.getOutputStream());
+                out.writeUTF("谢谢连接我："+server.getLocalSocketAddress()+"\nGoodbye!");
+                server.close();
+            }catch (SocketTimeoutException s){
+                System.out.println("Socket timed out!");
+                return;
+            }catch (IOException e){
+                e.printStackTrace();
+                break;
+            }
         }
     }
 }
