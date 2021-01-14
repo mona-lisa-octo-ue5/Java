e0b10dff4e240b2acfc7c7477ce5a70c6a30287d
石玉龙 <iosshiyulong@yeah.net>
2021-01-14 15:11:05 +0800

    创建一个简单的链表实例：

---
+++ src/com/java/fortgeschrittenesTutorial/JavaLinkedListDemo.java
@@ -0,0 +1,19 @@
+package com.java.fortgeschrittenesTutorial;
+
+import java.util.LinkedList;
+
+public class JavaLinkedListDemo {
+    public static void main(String[] args) {
+        add();
+
+    }
+
+    public static void add(){
+        LinkedList<String> sites=new LinkedList<String>();
+        sites.add("Google");
+        sites.add("Runoob");
+        sites.add("Taobao");
+        sites.add("Weibo");
+        System.out.println(sites);
+    }
+}
