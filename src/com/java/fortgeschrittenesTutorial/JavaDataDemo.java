f5b5ea395f79cabcc716bb3f083c87f511c7045d
石玉龙 <iosshiyulong@yeah.net>
2021-01-14 14:43:58 +0800

    通过Map.values()遍历所有的value，但不能遍历key

--- src/com/java/fortgeschrittenesTutorial/JavaDataDemo.java
+++ src/com/java/fortgeschrittenesTutorial/JavaDataDemo.java
@@ -26,6 +26,22 @@ public class JavaDataDemo {
         for (String key: map.keySet()){
             System.out.println("key= "+key+" and value= "+map.get(key));
         }
+        System.out.println("通过Map.entrySet使用iterator遍历key和value：");
+        Iterator<Map.Entry<String,String>> it=map.entrySet().iterator();
+        while (it.hasNext()){
+            Map.Entry<String,String> entry=it.next();
+            System.out.println("key= "+entry.getKey()+" add value= "+entry.getValue());
+        }
+
+        System.out.println("通过Map.entrySet遍历key和value");
+        for (Map.Entry<String,String> entry: map.entrySet()){
+            System.out.println("key= "+entry.getKey()+" and value = "+entry.getValue());
+        }
+
+        System.out.println("通过Map.values()遍历所有的value，但不能遍历key");
+        for (String v: map.values()){
+            System.out.println("value= "+v);
+        }
     }
     public static void arrayList(){
         List<String> list=new ArrayList<String>();
