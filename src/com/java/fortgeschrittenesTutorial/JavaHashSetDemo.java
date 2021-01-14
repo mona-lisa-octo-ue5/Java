460119b3c80a0637d5ec41d73f866581c3660fc3
石玉龙 <iosshiyulong@yeah.net>
2021-01-14 15:27:55 +0800

    迭代 HashSet

--- src/com/java/fortgeschrittenesTutorial/JavaHashSetDemo.java
+++ src/com/java/fortgeschrittenesTutorial/JavaHashSetDemo.java
@@ -5,15 +5,35 @@ import java.util.HashSet;
 public class JavaHashSetDemo {
     public static void main(String[] args) {
         add();
+        add2();
     }
+    public static void add2(){
+        HashSet<String> sites=new HashSet<String>();
+        sites.add("Google");
+        sites.add("Runoob");
+        sites.add("Taobao");
+        sites.add("Zhihu");
+        sites.add("Runoob");
+        System.out.println(sites);
+        System.out.println(sites.size());
+        for (String i: sites){
+            System.out.println(i);
+        }
+        
 
+    }
     public static void add(){
         HashSet<String> sites=new HashSet<String>();
         sites.add("Google");
         sites.add("Runoob");
-        sites.add("Tapbao");
+        sites.add("Taobao");
         sites.add("Zhihu");
         sites.add("Runoob");
+        System.out.println(sites);
+        System.out.println(sites.contains("Taobao"));
+        sites.remove("Taobao");
+        System.out.println(sites);
+        sites.clear();
         System.out.println(sites);
     }
 }
