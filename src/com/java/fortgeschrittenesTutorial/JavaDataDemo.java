075d8b51e0077d6e0c1adf046d067ba4dac2c482
石玉龙 <iosshiyulong@yeah.net>
2021-01-14 14:14:41 +0800

    Properties

--- src/com/java/fortgeschrittenesTutorial/JavaDataDemo.java
+++ src/com/java/fortgeschrittenesTutorial/JavaDataDemo.java
@@ -4,11 +4,32 @@ import java.util.*;
 
 public class JavaDataDemo {
     public static void main(String[] args) {
+        properties();
         hashtable();
         map();
         vector();
         bitset();
         enumeration();
+    }
+
+    public static void properties(){
+        Properties capitals=new Properties();
+        Set states;
+        String str;
+        capitals.put("Illinois","Springfield");
+        capitals.put("Missouri","Jefferson City");
+        capitals.put("Washington", "Olympia");
+        capitals.put("California","Sacramento");
+        capitals.put("Indiana","Indianapolis");
+        states = capitals.keySet();
+        Iterator itr = states.iterator();
+        while (itr.hasNext()){
+            str=(String) itr.next();
+            System.out.println("The capital of "+str+" is "+capitals.getProperty(str)+".");
+        }
+        System.out.println();
+        str= capitals.getProperty("Florida","Not Found");
+        System.out.println("The capital of Florida is "+str+".");
     }
     public static void hashtable(){
         Hashtable balance=new Hashtable();
