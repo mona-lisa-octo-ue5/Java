c274474240a295412d9fc3bc8f77057c89b53c58
石玉龙 <iosshiyulong@yeah.net>
2021-01-14 14:50:15 +0800

    添加元素

---
+++ src/com/java/fortgeschrittenesTutorial/JavaArrayListDemo.java
@@ -0,0 +1,20 @@
+package com.java.fortgeschrittenesTutorial;
+
+import com.java.obj.A;
+
+import java.util.ArrayList;
+
+public class JavaArrayListDemo {
+    public static void main(String[] args) {
+        add();
+    }
+
+    public static void add(){
+        ArrayList<String> sites=new ArrayList<String>();
+        sites.add("Google");
+        sites.add("Runoob");
+        sites.add("Taobao");
+        sites.add("Weibo");
+        System.out.println(sites);
+    }
+}
