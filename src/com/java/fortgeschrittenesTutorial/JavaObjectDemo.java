be8b2ec40821e614a5832ccd1b072f757b82157a
石玉龙 <iosshiyulong@yeah.net>
2021-01-14 16:14:30 +0800

    Java Object notify() 方法

--- src/com/java/fortgeschrittenesTutorial/JavaObjectDemo.java
+++ src/com/java/fortgeschrittenesTutorial/JavaObjectDemo.java
@@ -1,10 +1,85 @@
 package com.java.fortgeschrittenesTutorial;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
 public class JavaObjectDemo implements Cloneable {
     String name;
     int likes;
+    private List synchedList;
+    public JavaObjectDemo(){
+        synchedList= Collections.synchronizedList(new LinkedList<>());
+    }
+
+    public String removeElement() throws InterruptedException{
+        synchronized (synchedList){
+            while (synchedList.isEmpty()){
+                System.out.println("List is empty...");
+                synchedList.wait();
+                System.out.println("Waiting...");
+            }
+            String element=(String) synchedList.remove(0);
+            return element;
+        }
+    }
+
+    public void addElement(String element){
+        System.out.println("Opening...");
+        synchronized (synchedList){
+            synchedList.add(element);
+            System.out.println("New Element: "+element+"'");
+            synchedList.notifyAll();
+            System.out.println("notifyAll called!");
+        }
+        System.out.println("Closing...");
+    }
 
     public static void main(String[] args) {
+
+        final JavaObjectDemo demo=new JavaObjectDemo();
+        Runnable runA=new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    String item=demo.removeElement();
+                    System.out.println(""+item);
+                }catch (InterruptedException ix){
+                    System.out.println(ix);
+                }
+                catch (Exception e){
+                    System.out.println(e);
+                }
+            }
+        };
+        Runnable runB=new Runnable() {
+            @Override
+            public void run() {
+                demo.addElement("Hello!");
+            }
+        };
+
+        try{
+            Thread threadA1=new Thread(runA, "Google");
+            threadA1.start();
+            Thread.sleep(500);
+            Thread threadA2=new Thread(runA,"Runoob");
+            threadA2.start();
+            Thread.sleep(500);
+            Thread threadB = new Thread(runB, "Taobao");
+            threadB.start();
+
+            Thread.sleep(1000);
+
+            threadA1.interrupt();
+            threadA2.interrupt();
+        }catch (InterruptedException x){
+
+        }
+
+        System.out.println();
+
         JavaObjectDemo o=new JavaObjectDemo();
         o.name="Runoob";
         o.likes=111;
