94a2ab6e302f359c1be189b1731e5d4bcc12854f
石玉龙 <iosshiyulong@yeah.net>
2021-01-15 16:09:31 +0800

    Java 中我们可以使用 StringTokennizer 设置不同分隔符来分隔字符串，默认的分隔符是：空格、制表符（\t）、换行符(\n）、回车符（\r）。

--- src/com/java/fortgeschrittenesTutorial/JavaStringDemo.java
+++ src/com/java/fortgeschrittenesTutorial/JavaStringDemo.java
@@ -1,7 +1,10 @@
 package com.java.fortgeschrittenesTutorial;
 
+import java.util.StringTokenizer;
+
 public class JavaStringDemo {
     public static void main(String[] args) {
+        m9();
         m8();
         m7();
         m6();
@@ -11,6 +14,19 @@ public class JavaStringDemo {
         System.out.println(removeCharAt(str,3));
     }
 
+    public static void m9(){
+        String str = "This is String , split by StringTokenizer, created by runoob";
+        StringTokenizer st = new StringTokenizer(str);
+        System.out.println("----- 通过空格分隔 -----");
+        while (st.hasMoreElements()) {
+            System.out.println(st.nextElement());
+        }
+        System.out.println("----- 通过逗号分隔 -----");
+        StringTokenizer st2 = new StringTokenizer(str, ",");
+        while (st2.hasMoreElements()) {
+            System.out.println(st2.nextElement());
+        }
+    }
     public static void m8(){
         String str = "www-runoob-com";
         String[] temp;
