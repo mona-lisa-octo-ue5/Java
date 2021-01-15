f0298bcb209c6eb32d9c7f642811bc7d11cee077
石玉龙 <iosshiyulong@yeah.net>
2021-01-15 14:34:43 +0800

    获得applet参数

--- src/com/java/fortgeschrittenesTutorial/JavaAppletDemo.java
+++ src/com/java/fortgeschrittenesTutorial/JavaAppletDemo.java
@@ -7,6 +7,26 @@ class HelloWorldApplet extends Applet{
     public void paint(Graphics g){
         g.drawString("Hello World",25,50);
     }
+    int squareSize=50;
+    public void init(){
+        String squareSizeParam=getParameter("squareSize");
+        parseSquareSize(squareSizeParam);
+        String colorParam=getParameter("Color");
+        Color fg=parseColor(colorParam);
+        setBackground(Color.black);
+        setForeground(fg);
+    }
+    private void parseSquareSize(String param){
+        if (param==null) return;
+        try{
+            squareSize=Integer.parseInt(param);
+        }catch (Exception e){
+            
+        }
+    }
+    private Color parseColor(String param){}
+
+
 }
 public class JavaAppletDemo {
     public static void main(String[] args) {
