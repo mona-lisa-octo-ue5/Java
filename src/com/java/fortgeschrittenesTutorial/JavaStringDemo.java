82a7e566d038d77e6e3826842b5ee7ff53527cad
石玉龙 <iosshiyulong@yeah.net>
2021-01-15 17:07:53 +0800

    Java 实例 - 连接字符串

--- src/com/java/fortgeschrittenesTutorial/JavaStringDemo.java
+++ src/com/java/fortgeschrittenesTutorial/JavaStringDemo.java
@@ -5,6 +5,7 @@ import java.util.StringTokenizer;
 
 public class JavaStringDemo {
     public static void main(String[] args) {
+        m17();
         m16();
         m15();
         m12();
@@ -19,6 +20,27 @@ public class JavaStringDemo {
         System.out.println(removeCharAt(str,3));
     }
 
+    public static void m17() {
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < 50000; i++) {
+            String result = "This is" + "testing the" + "difference" + "between" + "String" + "and" + "StringBuffer";
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("字符串连接" + " - 使用 + 操作符 ： " + (endTime - startTime) + " ms");
+        long startTime1 = System.currentTimeMillis();
+        for (int i = 0; i < 50000; i++) {
+            StringBuffer result = new StringBuffer();
+            result.append("This is");
+            result.append("testing the");
+            result.append("difference");
+            result.append("between");
+            result.append("String");
+            result.append("and");
+            result.append("StringBuffer");
+        }
+        long endTime1 = System.currentTimeMillis();
+        System.out.println("字符串连接" + " - 使用 StringBuffer : " + (endTime1 - startTime1) + " ms");
+    }
     public static void m16() {
         double e = Math.E;
         System.out.format("%f%n", e);
