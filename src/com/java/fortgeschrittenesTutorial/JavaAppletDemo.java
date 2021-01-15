fe96ecf001a2b2c8c9db14f54e3ac72a40a2140c
石玉龙 <iosshiyulong@yeah.net>
2021-01-15 14:48:40 +0800

    事件处理 Applet 类从 Container 类继承了许多事件处理方法。Container 类定义了几个方法，例如：processKeyEvent() 和processMouseEvent()，用来处理特别类型的事件，还有一个捕获所有事件的方法叫做 processEvent。 为了响应一个事件，Applet 必须重写合适的事件处理方法。

--- src/com/java/fortgeschrittenesTutorial/JavaAppletDemo.java
+++ src/com/java/fortgeschrittenesTutorial/JavaAppletDemo.java
@@ -2,6 +2,9 @@ package com.java.fortgeschrittenesTutorial;
 
 import java.applet.Applet;
 import java.awt.*;
+import java.awt.desktop.AppEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 class HelloWorldApplet extends Applet{
     public void paint(Graphics g){
@@ -21,12 +24,51 @@ class HelloWorldApplet extends Applet{
         try{
             squareSize=Integer.parseInt(param);
         }catch (Exception e){
-            
+
         }
     }
     private Color parseColor(String param){}
+}
+class ExampleEventHandling extends Applet implements MouseListener{
+    StringBuffer strBuffer;
+    public void init(){
+        addMouseListener(this);
+        strBuffer=new StringBuffer();
+        addItem("initializing the applet ");
+    }
+    public void start(){
+        addItem("starting the applet ");
+    }
+    public void stop(){
+        addItem("stopping the applet ");
+    }
+    public void destroy(){
+        addItem("unloading the applet ");
+    }
+    void addItem(String word){
+        System.out.println(word);
+        strBuffer.append(word);
+        repaint();
+    }
+    public void paint(Graphics g){
+        g.drawRect(0,0,getWidth()-1,getHeight()-1);
+        g.drawString(strBuffer.toString(),10,20);
+    }
+    public void mouseEntered(MouseEvent event){
 
+    }
+    public void mouseExited(MouseEvent event){
 
+    }
+    public void mousePressed(MouseEvent event){
+
+    }
+    public void mouseReleased(MouseEvent event){
+
+    }
+    public void mouseClicked(MouseEvent event){
+        addItem("mouse clicked! ");
+    }
 }
 public class JavaAppletDemo {
     public static void main(String[] args) {
