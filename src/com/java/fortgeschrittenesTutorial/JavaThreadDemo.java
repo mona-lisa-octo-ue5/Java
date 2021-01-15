63a0d7e34b44584ca2b946cfb1326a52a81c97c3
石玉龙 <iosshiyulong@yeah.net>
2021-01-15 14:01:13 +0800

    通过继承Thread来创建线程

--- src/com/java/fortgeschrittenesTutorial/JavaThreadDemo.java
+++ src/com/java/fortgeschrittenesTutorial/JavaThreadDemo.java
@@ -30,11 +30,45 @@ class RunnableDemo implements Runnable {
         }
     }
 }
+class ThreadDemo extends Thread{
+    private Thread t;
+    private String threadName;
+    ThreadDemo(String name){
+        threadName=name;
+        System.out.println("Creating "+threadName);
+    }
+
+    public void run() {
+        System.out.println("Running "+threadName);
+        try{
+            for (int i=4;i>0;i--){
+                System.out.println("Thread: "+threadName+","+i);
+                Thread.sleep(50);
+            }
+        }catch (InterruptedException e){
+            System.out.println("Thread: "+threadName+" interrupted.");
+        }
+        System.out.println("Thread "+threadName+" exiting.");
+    }
+
+    public void start(){
+        System.out.println("Starting "+threadName);
+        if (t==null){
+            t=new Thread(this,threadName);
+            t.start();
+        }
+    }
+}
 public class JavaThreadDemo {
     public static void main(String[] args) {
         RunnableDemo r=new RunnableDemo("Thread-1");
         r.start();
         RunnableDemo r2=new RunnableDemo("Thread-2");
         r2.start();
+        ThreadDemo t=new ThreadDemo("Thread-1-1");
+        t.start();
+        ThreadDemo t2=new ThreadDemo("Thread-2-2");
+        t2.start();
+
     }
 }
