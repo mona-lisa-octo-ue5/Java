ae681071bfcda0df13c36487e7e9d14ce025c694
石玉龙 <iosshiyulong@yeah.net>
2021-01-15 17:47:54 +0800

    no message

--- src/com/java/fortgeschrittenesTutorial/JavaArrayDemo.java
+++ src/com/java/fortgeschrittenesTutorial/JavaArrayDemo.java
@@ -4,9 +4,28 @@ import java.util.Arrays;
 
 public class JavaArrayDemo {
     public static void main(String[] args) {
+        m2();
         m1();
     }
 
+    public static int[] insertElement(int original[], int element, int index) {
+        int length = original.length;
+        int destination[] = new int[length + 1];
+        System.arraycopy(original, 0, destination, 0, index);
+        destination[index] = element;
+        System.arraycopy(original, index, destination, index + 1, length - index);
+        return destination;
+    }
+    public static void m2() {
+        int array[] = { 2, 5, -2, 6, -3, 8, 0, -7, -9, 4 };
+        Arrays.sort(array);
+        printArray("数组排序", array);
+        int index = Arrays.binarySearch(array, 1);
+        System.out.println("元素 1 所在位置（负数为不存在）：" + index);
+        int newIndex = -index - 1;
+        array = insertElement(array, 1, newIndex);
+        printArray("数组添加元素 1", array);
+    }
     public static void printArray(String message, int array[]) {
         System.out.println(message + ": [length: " + array.length + "]");
         for (int i = 0; i < array.length; i++) {
