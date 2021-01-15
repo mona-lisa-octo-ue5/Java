fe1edb32631f400eefa4fcab405764662c8f256d
石玉龙 <iosshiyulong@yeah.net>
2021-01-15 15:24:54 +0800

    Java 实例 - 删除字符串中的一个字符

--- src/com/java/fortgeschrittenesTutorial/JavaStringDemo.java
+++ src/com/java/fortgeschrittenesTutorial/JavaStringDemo.java
@@ -3,8 +3,12 @@ package com.java.fortgeschrittenesTutorial;
 public class JavaStringDemo {
     public static void main(String[] args) {
         m4();
+        String str="this is Java";
+        System.out.println(removeCharAt(str,3));
     }
-
+    public static String removeCharAt(String s,int pos){
+        return s.substring(0,pos)+s.substring(pos+1);
+    }
     public static void m4(){
         String s="Hello world ,Hello Runoob";
         int l=s.lastIndexOf("Runoob");
