098d360af952c53ac953ff023e9c9982ac72029d
石玉龙 <iosshiyulong@yeah.net>
2021-01-15 15:29:04 +0800

    Java 实例 - 字符串替换

--- src/com/java/fortgeschrittenesTutorial/JavaStringDemo.java
+++ src/com/java/fortgeschrittenesTutorial/JavaStringDemo.java
@@ -2,9 +2,16 @@ package com.java.fortgeschrittenesTutorial;
 
 public class JavaStringDemo {
     public static void main(String[] args) {
+        m5();
         m4();
         String str="this is Java";
         System.out.println(removeCharAt(str,3));
+    }
+    public static void m5(){
+        String str="Hello World";
+        System.out.println(str.replace('H','W'));
+        System.out.println(str.replaceFirst("He","Wa"));
+        System.out.println(str.replaceAll("He","Ha"));
     }
     public static String removeCharAt(String s,int pos){
         return s.substring(0,pos)+s.substring(pos+1);
