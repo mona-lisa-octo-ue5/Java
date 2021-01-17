b1d1ca528b1a68695a15e7322363f9a46dc007bf
石玉龙 <iosshiyulong@yeah.net>
2021-01-17 16:54:32 +0800

    no message

--- src/com/java/fortgeschrittenesTutorial/Main.java
+++ src/com/java/fortgeschrittenesTutorial/Main.java
@@ -4,7 +4,7 @@ import java.net.URLConnection;
 import java.util.Date;
 import java.text.SimpleDateFormat;
 
-public class Main {
+public class Main extends Thread {
     public static void main(String[] args) throws Exception {
         URL u = new URL("https://www.runoob.com/java/java-examples.html");
         URLConnection uc = u.openConnection();
@@ -12,5 +12,23 @@ public class Main {
         uc.setUseCaches(false);
         long timestamp = uc.getLastModified();
         System.out.println("test.html 文件最后修改时间 :" + ft.format(new Date(timestamp)));
+
+        Main tt=new Main();
+        tt.setName("Thread");
+        System.out.println("before start() ");
+
+
+    }
+
+    public void run() {
+        for (int i=0;i<10;i++){
+            printMsg();
+        }
+    }
+
+    public void printMsg(){
+        Thread t=Thread.currentThread();
+        String name=t.getName();
+        System.out.println("name="+name);
     }
 }
