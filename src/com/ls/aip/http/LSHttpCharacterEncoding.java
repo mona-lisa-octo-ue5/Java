fc512e8a9097e566a7e2a7baa76a1f6bdbf4eaef
石玉龙 <iosshiyulong@yeah.net>
2021-01-18 11:27:10 +0800

    LSHttpCharacterEncoding

---
+++ src/com/ls/aip/http/LSHttpCharacterEncoding.java
@@ -0,0 +1,13 @@
+package com.ls.aip.http;
+
+import java.security.PublicKey;
+
+public class LSHttpCharacterEncoding {
+    public static final String DEFAULT_ENCODING = "UTF8";
+    public static final String ENCODE_UTF8 = "UTF8";
+    public static final String ENCODE_GBK = "GBK";
+
+    public LSHttpCharacterEncoding() {
+
+    }
+}
