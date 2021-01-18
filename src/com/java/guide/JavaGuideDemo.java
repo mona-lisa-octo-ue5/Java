1583f7455a648fba07acc29493cd1817d64d7e14
石玉龙 <iosshiyulong@yeah.net>
2021-01-18 11:16:17 +0800

    封装ls sdk

---
+++ src/com/java/guide/JavaGuideDemo.java
@@ -0,0 +1,7 @@
+package com.java.guide;
+
+import java.util.ArrayList;
+
+public class JavaGuideDemo {
+
+}
---
+++ src/com/ls/aip/http/LSHttpMethodName.java
@@ -0,0 +1,7 @@
+package com.ls.aip.http;
+
+public class LSHttpMethodName {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
