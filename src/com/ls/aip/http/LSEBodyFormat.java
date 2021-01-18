b692d80a3ab56546a6e997381eecec528efceabe
石玉龙 <iosshiyulong@yeah.net>
2021-01-18 14:11:42 +0800

    LSEBodyFormat

---
+++ src/com/ls/aip/http/LSEBodyFormat.java
@@ -0,0 +1,11 @@
+package com.ls.aip.http;
+
+public enum LSEBodyFormat {
+    FORM_KV,
+    RAW_JSON,
+    RAW_JSON_ARRAY;
+
+    private LSEBodyFormat() {
+
+
+    }}
---
+++ src/com/ls/aip/http/LSHeaders.java
@@ -0,0 +1,33 @@
+package com.ls.aip.http;
+
+public interface LSHeaders {
+    String AUTHORIZATION = "Authorization";
+    String CACHE_CONTROL = "Cache-Control";
+    String CONTENT_DISPOSITION = "Content-Disposition";
+    String CONTENT_ENCODING = "Content-Encoding";
+    String CONTENT_LENGTH = "Content-Length";
+    String CONTENT_MD5 = "Content-MD5";
+    String CONTENT_RANGE = "Content-Range";
+    String CONTENT_TYPE = "Content-Type";
+    String DATE = "Date";
+    String ETAG = "ETag";
+    String EXPIRES = "Expires";
+    String HOST = "Host";
+    String LAST_MODIFIED = "Last-Modified";
+    String RANGE = "Range";
+    String SERVER = "Server";
+    String TRANSFER_ENCODING = "Transfer-Encoding";
+    String USER_AGENT = "User-Agent";
+    String BCE_ACL = "x-bce-acl";
+    String BCE_CONTENT_SHA256 = "x-bce-content-sha256";
+    String BCE_COPY_METADATA_DIRECTIVE = "x-bce-metadata-directive";
+    String BCE_COPY_SOURCE = "x-bce-copy-source";
+    String BCE_COPY_SOURCE_IF_MATCH = "x-bce-copy-source-if-match";
+    String BCE_DATE = "x-bce-date";
+    String BCE_DEBUG_ID = "x-bce-debug-id";
+    String BCE_PREFIX = "x-bce-";
+    String BCE_REQUEST_ID = "x-bce-request-id";
+    String BCE_SECURITY_TOKEN = "x-bce-security-token";
+    String BCE_STORAGE_CLASS = "x-bce-storage-class";
+    String BCE_USER_METADATA_PREFIX = "x-bce-meta-";
+}
