fdbd7ad26ad2d1a59aac4f56567af0043e472457
石玉龙 <iosshiyulong@yeah.net>
2021-01-19 17:48:53 +0800

    jedis

--- src/com/ls/aip/util/LSAipClientConfiguration.java
+++ src/com/ls/aip/util/LSAipClientConfiguration.java
@@ -1,5 +1,51 @@
 package com.ls.aip.util;
 
-public class LSAipClientConfiguration {
+import java.lang.reflect.Proxy;
+import java.lang.reflect.Type;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
 
+public class LSAipClientConfiguration {
+    private int connectionTimeoutMillis;
+    private int socketTimeoutMills;
+    private Proxy proxy;
+    public LSAipClientConfiguration() {
+        this.connectionTimeoutMillis=0;
+        this.socketTimeoutMills=0;
+        this.proxy=Proxy.NO_PROXY;
+    }
+    public LSAipClientConfiguration(int connectionTimeoutMillis, int socketTimeoutMills, Proxy proxy) {
+        this.connectionTimeoutMillis=connectionTimeoutMillis;
+        this.socketTimeoutMills=socketTimeoutMills;
+        this.proxy=proxy;
+    }
+
+    public int getConnectionTimeoutMillis() {
+        return this.connectionTimeoutMillis;
+    }
+
+    public void setConnectionTimeoutMillis(int connectionTimeoutMillis) {
+        this.connectionTimeoutMillis = connectionTimeoutMillis;
+    }
+
+    public int getSocketTimeoutMills() {
+        return this.socketTimeoutMills;
+    }
+
+    public void setSocketTimeoutMills(int socketTimeoutMills) {
+        this.socketTimeoutMills = socketTimeoutMills;
+    }
+
+    public Proxy getProxy() {
+        return this.proxy;
+    }
+
+    public void setProxy(Proxy proxy) {
+        this.proxy = proxy;
+    }
+
+    public void setProxy(String host, int port, Type type){
+        SocketAddress addr = new InetSocketAddress(host, port);
+        this.proxy=new Proxy(type, addr);
+    }
 }
---
+++ src/com/ls/apns/LSFeedbackService.java
@@ -0,0 +1,5 @@
+package com.ls.apns;
+
+public class LSFeedbackService {
+    public LSFeedbackService(ApnsC)
+}
