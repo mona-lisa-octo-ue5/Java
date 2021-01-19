cafaf580538a8135d1cfb242ada741a898d1e67d
石玉龙 <iosshiyulong@yeah.net>
2021-01-19 18:09:47 +0800

    Redis Java String(字符串) 实例

--- src/com/db/redis/RedisJava.java
+++ src/com/db/redis/RedisJava.java
@@ -6,6 +6,8 @@ public class RedisJava {
     public static void main(String[] args) {
         Jedis jedis = new Jedis("localhost");
         System.out.println("连接成功");
-        System.out.println("服务正在运行：" + jedis.ping());
+//        System.out.println("服务正在运行：" + jedis.ping());
+        jedis.set("runoobkey","www.runoob.com");
+        System.out.println("redis 存储的字符串为： "+jedis.get("runoobkey"));
     }
 }
