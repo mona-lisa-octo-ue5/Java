d71191dce9e9a470a3d65b102a34eb3f26d5571e
石玉龙 <iosshiyulong@yeah.net>
2021-01-20 14:22:17 +0800

    LSAipClientConst

---
+++ src/com/ls/aip/util/LSAipClientConst.java
@@ -0,0 +1,23 @@
+package com.ls.aip.util;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+public class LSAipClientConst {
+    public static final Integer BCE_AUTH_EXPIRE_IN_SECONDS = 1800;
+    public static final HashSet<String> BCE_HEADER_TO_SIGN = new HashSet(Arrays.asList("host","content-md5","content-length","content-type"));
+    public static final String OAUTH_URL = "https://aip.baidubce.com/oauth/2.0/token";
+    public static final String BCE_PREFIX = "x-bce-";
+    public static final String DEFAULT_ENCODING = "UTF8";
+    public static final String ENCODING_GBK = "GBK";
+    public static final List<String> AI_ACCESS_RIGHT = Arrays.asList("audio_voice_assistant_get", "audio_tts_post", "brain_all_scope");
+    public static final Integer IAM_ERROR_CODE = 14;
+    public static final Integer OPENAPI_NO_ACCESS_ERROR_CODE = 6;
+    public static final String OPENAPI_NO_ACCESS_ERROR_MSG = "No permission to access data";
+    public static final String LOG4J_CONF_PROPERTY = "aip.log4j.conf";
+
+    public LSAipClientConst(){
+
+    }
+}
