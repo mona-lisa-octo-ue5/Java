68a096b49ba92990ade30fcef209d8d417343622
石玉龙 <iosshiyulong@yeah.net>
2021-01-20 14:28:50 +0800

    LSBodyAnalysisConsts

---
+++ src/com/ls/aip/bodyanalysis/LSBodyAnalysisConsts.java
@@ -0,0 +1,17 @@
+package com.ls.aip.bodyanalysis;
+
+import java.security.PublicKey;
+
+public class LSBodyAnalysisConsts {
+    static final String BODY_ANALYSIS = "https://aip.baidubce.com/rest/2.0/image-classify/v1/body_analysis";
+    static final String BODY_ATTR = "https://aip.baidubce.com/rest/2.0/image-classify/v1/body_attr";
+    static final String BODY_NUM = "https://aip.baidubce.com/rest/2.0/image-classify/v1/body_num";
+    static final String GESTURE = "https://aip.baidubce.com/rest/2.0/image-classify/v1/gesture";
+    static final String BODY_SEG = "https://aip.baidubce.com/rest/2.0/image-classify/v1/body_seg";
+    static final String DRIVER_BEHAVIOR = "https://aip.baidubce.com/rest/2.0/image-classify/v1/driver_behavior";
+    static final String BODY_TRACKING = "https://aip.baidubce.com/rest/2.0/image-classify/v1/body_tracking";
+    static final String HAND_ANALYSIS = "https://aip.baidubce.com/rest/2.0/image-classify/v1/hand_analysis";
+    public LSBodyAnalysisConsts() {
+
+    }
+}
